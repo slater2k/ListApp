a4422f90a2ef633ec10767c59c6df0edbe6799e5
Cleancookie <pokemon999101@hotmail.co.uk>
2021-03-07 16:13:50 +0000

    ✨ Install nodemon 🔥 Remove static file serving ✨ Add 404

--- BE/app.js
+++ BE/app.js
@@ -11,8 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
+app.all("*", (req, res) => res.status(404).json({error: 'Not found'}) );
 
 module.exports = app;
