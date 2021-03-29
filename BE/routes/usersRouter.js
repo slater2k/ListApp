584847b25a7e928e0532a4d58a82e41bd8c3b201
Cleancookie <pokemon999101@hotmail.co.uk>
2021-03-29 18:26:16 +0100

    💥 Remove old api

--- BE/routes/usersRouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const usersController = require("../app/Controllers/usersController");
-
-router.get("/", usersController.getUsers);
-router.post("/register", usersController.createUser);
-
-module.exports = router;
