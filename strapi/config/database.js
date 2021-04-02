40b5576f9b1cc1f6942dc0b13d0a60e737d1fabe
Cleancookie <pokemon999101@hotmail.co.uk>
2021-04-02 15:34:37 +0100

    📦 MySql credentials

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,11 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
       },
       options: {
         useNullAsDefault: true,
